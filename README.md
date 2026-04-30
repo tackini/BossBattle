9f9a0c43d3777f0ffe99496044a4ae8373041253
a24i296 <a24i296mk@ous.jp>
2026-05-01 00:46:12 +0900

    初期コミット

---
+++ README.md
@@ -0,0 +1,3 @@
+# BossBattle
+
+Developed with Unreal Engine 5
---
+++ Source/BossBattle/BossBattle.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class BossBattle : ModuleRules
+{
+	public BossBattle(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
